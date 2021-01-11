7c99dde5640866b465534f142838a9d7e1d0c7ca
Silanah <oktawia.czuczwara90@gmail.com>
2021-01-11 16:12:42 +0100

    added files for Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
 import HomeHeader from './HomeHeader';
 // import HomeThreeColumns from './HomeThreeColumns';
+// import Home4Steps from './Home4Steps';
+// import Home4Images from './Home4Images';
+// import HomeGiveAway from './HomeGiveAway';
+// import HomeAboutUs from './HomeAboutUs';
+// import HomeWhoWeHelp from './HomeWhoWeHelp';
+// import HomeContactForm from './HomeContactForm';
+// import HomeFooter from './HomeFooter';
 
 const Home = () => {
     return(
         <>
             <HomeHeader />
             {/* <HomeThreeColumns /> */}
+            {/* <Home4Steps /> */}
+            {/* <Home4Images /> */}
+            {/* <HomeGiveAway /> */}
+            {/* <HomeAboutUs /> */}
+            {/* <HomeWhoWeHelp /> */}
+            {/* <HomeContactForm /> */}
+            {/* <HomeFooter /> */}
         </>
     );
 };
