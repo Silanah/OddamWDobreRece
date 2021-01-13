321f07046a3f73d9185fc37e0c5b16dde1b5b455
Silanah <oktawia.czuczwara90@gmail.com>
2021-01-13 16:17:54 +0100

    finished 3rd section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import HomeHeader from './HomeHeader';
 import HomeThreeColumns from './HomeThreeColumns';
 import Home4Steps from './Home4Steps';
-import Home4Images from './Home4Images';
-import HomeGiveAway from './HomeGiveAway';
 import HomeAboutUs from './HomeAboutUs';
 import HomeWhoWeHelp from './HomeWhoWeHelp';
 import Footer from '../Footer';
@@ -16,8 +14,6 @@ class Home extends Component {
                 <HomeHeader />
                 <HomeThreeColumns />
                 <Home4Steps />
-                <Home4Images />
-                <HomeGiveAway />
                 <HomeAboutUs />
                 <HomeWhoWeHelp />
                 <Footer />
